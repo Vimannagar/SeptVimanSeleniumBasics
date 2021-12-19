6c9f6545be33ab4f6181cdc200eaae5bf4997d43
Gourav Jawale <velocity.ctc@gmail.com>
2021-12-19 09:43:37 +0530

    TestNG

---
+++ src/testngdiscussion/TestCase.java
@@ -0,0 +1,29 @@
+package testngdiscussion;
+
+import org.testng.annotations.Test;
+
+public class TestCase {
+	
+	
+	@Test
+	public void loginTest()
+	{
+		System.out.println("Login test case");
+	}
+	
+	
+	@Test
+	public void home()
+	{
+		System.out.println("Home test case");
+	}
+	
+	
+	@Test
+	public void a()
+	{
+		System.out.println("A test case");
+	}
+
+
+}
